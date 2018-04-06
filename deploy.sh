dfbc438582e33f1cd5c01b6ee2415ed1b7540d8a
Kyle Buchanan <kylebuch8@gmail.com>
2018-04-06 10:00:11 -0400

    updates for deploying

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@ echo "==> Building the application"
 bower install && npm install
 polymer build
 
+# sed -i -e "s/return'ws:\/\/localhost:8080\/game'/return location.protocol==='https:'?'wss:\/\/'+location.hostname+'\/game':'ws:\/\/'+location.hostname+'\/game'/g" build/es5-bundled/src/sh-app/sh-app.html
+sed -i -e "s/return'ws:\/\/localhost:8080\/game'/return location.protocol==='https:'?'wss:\/\/scavenger-hunt-microservice-scavenger-hunt-microservice.apps.summit-aws.sysdeseng.com\/game':'ws:\/\/scavenger-hunt-microservice-scavenger-hunt-microservice.apps.summit-aws.sysdeseng.com\/game'/g" build/es5-bundled/src/sh-app/sh-app.html
+rm build/es5-bundled/src/sh-app/sh-app.html-e
+
 export APP_NAME=scavenger-hunt-game
 echo "==> Creating build and pushing bits"
 oc new-build --binary --name=${APP_NAME} -l app=${APP_NAME}
