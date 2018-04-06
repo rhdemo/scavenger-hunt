057857e5b48248017217100ac87adb7821bf2ad5
Kyle Buchanan <kylebuch8@gmail.com>
2018-04-06 10:31:54 -0400

    updating replacement in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ bower install && npm install
 polymer build
 
 # sed -i -e "s/return'ws:\/\/localhost:8080\/game'/return location.protocol==='https:'?'wss:\/\/'+location.hostname+'\/game':'ws:\/\/'+location.hostname+'\/game'/g" build/es5-bundled/src/sh-app/sh-app.html
-sed -i -e "s/return'ws:\/\/localhost:8080\/game'/return location.protocol==='https:'?'wss:\/\/scavenger-hunt-microservice-scavenger-hunt-microservice.apps.summit-aws.sysdeseng.com\/game':'ws:\/\/scavenger-hunt-microservice-scavenger-hunt-microservice.apps.summit-aws.sysdeseng.com\/game'/g" build/es5-bundled/src/sh-app/sh-app.html
+sed -i -e "s/'ws:\/\/localhost:8080\/game'/'wss:\/\/scavenger-hunt-microservice-scavenger-hunt-microservice.apps.summit-aws.sysdeseng.com\/game'/g" build/es5-bundled/src/sh-app/sh-app.html
 rm build/es5-bundled/src/sh-app/sh-app.html-e
 
 export APP_NAME=scavenger-hunt-game
